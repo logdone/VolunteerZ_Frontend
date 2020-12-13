4a9a0f3a81685b2cd8bdfbf980723014224950fc
Logdone <firas.bouchami@gmail.com>
2020-12-13 19:05:24 +0100

    //Passing data event

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { EventContentPage } from './pages/entities/event/event-content/event-content.page';
+import { EventResolve } from './pages/entities/event/event.module';
 
 const routes: Routes = [
   { path: '', loadChildren: './pages/welcome/welcome.module#WelcomePageModule' },
@@ -9,7 +10,10 @@ const routes: Routes = [
   { path: 'signup', loadChildren: './pages/signup/signup.module#SignupPageModule' },
   { path: 'accessdenied', redirectTo: '', pathMatch: 'full' },
   {
-    path: 'event-content',
+    path: 'event-content/:id',
+    resolve: {
+      data: EventResolve,
+    },
     component: EventContentPage
   },
 
--- src/app/pages/entities/event/event-content/event-content.page.ts
+++ src/app/pages/entities/event/event-content/event-content.page.ts
@@ -1,4 +1,8 @@
+import { EventService } from 'src/app/pages/entities/event/event.service';
+import { Event } from './../event.model';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-event-content',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-content.page.scss'],
 })
 export class EventContentPage implements OnInit {
+  event: Event = {};
 
-  constructor() { }
+  constructor(    
+    private navController: NavController,
+    private eventService: EventService,
+    private activatedRoute: ActivatedRoute,
+    private alertController: AlertController) 
+    { }
 
   ngOnInit() {
+    this.activatedRoute.data.subscribe((response) => {
+      this.event = response.data;
+      console.log(this.event);
+    });
   }
 
 }
