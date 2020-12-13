968e8f65aba7fa06b1e69d178d2532cf797c6999
wSoltani <w.soltani@almaviva.tn>
2020-12-13 18:48:34 +0100

    App Routing Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { EventContentPage } from './pages/entities/event/event-content/event-content.page';
 
 const routes: Routes = [
   { path: '', loadChildren: './pages/welcome/welcome.module#WelcomePageModule' },
   { path: 'tabs', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
   { path: 'signup', loadChildren: './pages/signup/signup.module#SignupPageModule' },
-  { path: 'accessdenied', redirectTo: '', pathMatch: 'full' },  {
+  { path: 'accessdenied', redirectTo: '', pathMatch: 'full' },
+  {
     path: 'event-content',
-    loadChildren: () => import('./pages/entities/event/event-content/event-content.module').then( m => m.EventContentPageModule)
+    component: EventContentPage
   },
 
 
