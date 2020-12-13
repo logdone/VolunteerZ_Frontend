fd60fedfd37866952907ff83f6795aed20b2cc62
Logdone <firas.bouchami@gmail.com>
2020-12-13 20:15:48 +0100

    report comment + account in event

--- src/app/pages/entities/event/event-content/event-content.page.ts
+++ src/app/pages/entities/event/event-content/event-content.page.ts
@@ -3,6 +3,7 @@ import { Event } from './../event.model';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavController, AlertController } from '@ionic/angular';
+import { AccountService } from 'src/app/services/auth/account.service';
 
 @Component({
   selector: 'app-event-content',
@@ -11,23 +12,44 @@ import { NavController, AlertController } from '@ionic/angular';
 })
 export class EventContentPage implements OnInit {
   event: Event = {};
-
+  account: Account;
   constructor(    
     private navController: NavController,
     private eventService: EventService,
     private activatedRoute: ActivatedRoute,
+    private accountService: AccountService,
     private alertController: AlertController) 
     { }
 
   ngOnInit() {
+    console.log("in init");
     this.activatedRoute.data.subscribe((response) => {
       this.event = response.data;
       console.log(this.event);
     });
+    this.accountService.identity().then((account) => {
+      if (account === null) {
+        this.goBackToHomePage();
+      } else {
+        this.account = account;
+        console.log("this account name "+this.account.name);
+      }
+    });
+
   }
 
   goBack(){
     this.navController.back();
   }
 
+
+
+  private goBackToHomePage(): void {
+    this.navController.navigateBack('');
+  }
+
+  reportComment(){
+    console.log("Hello");
+  }
+  
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -79,5 +79,6 @@ export class HomePage implements OnInit {
         }
       );
   }
-  
+
+
 }
